616fcc9e6e8f340f8deecb4e31b792640e3c81a7
CBT <two_punch@163.com>
2022-03-22 11:22:59 +0800

    update-huamwei-homework

--- Huawei/ftrace-homework/litetrace.sh
+++ Huawei/ftrace-homework/litetrace.sh
@@ -2,11 +2,6 @@
 
 cd /sys/kernel/debug/tracing
 
-echo 0 > tracing_on
-echo function > current_tracer
-echo > trace
-echo 1 > tracing_on
-echo 0 > tracing_on
 #cat trace
 
 
@@ -19,10 +14,12 @@ do
 			echo > trace
 			echo 0 > tracing_on
 			echo "$OPTARG" > current_tracer
-			echo 1 > tracing_on
-			echo 0 > tracing_on;;
+			echo 1 > tracing_on;;
 		f)
-			echo $OPTARG > set_ftrace_filter;;
+			echo 0 > tracing_on
+			echo > set_ftrace_filter
+			echo $OPTARG > set_ftrace_filter
+			echo 1 > tracing_on;;
 		c) 
 			cat $OPTARG;;
 		?)	
