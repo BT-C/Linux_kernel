97eaf4e727ba614f8322a9e40548f08fc445f6da
CBT <two_punch@163.com>
2022-03-29 21:44:14 +0800

    update-systemtap-homework

---
+++ Huawei/systemtap-homework/test_malloc/memleak.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+	int count = 1000;
+	for (int i = 0; i < count; i ++){
+		int length = 100;
+		int *value = (int *)malloc(sizeof(int) * length);
+		printf("pid : %d, address : 0x%x\n", getpid(), value);
+		sleep(1);
+
+		int *temp = (int *)malloc(sizeof(int) * length);
+		sleep(2);
+		free(temp);
+		//free(value);
+	}
+	return 0;	
+}
